df1c3c5cdd8d104f94cadd1ef6ab812c1668e3c5
Juan Sebastian Lopez Galindo <juan.lopez.galindo@pi.edu.co>
2022-10-28 17:20:32 -0500

    new upgrades and skills among others added

---
+++ src/main/resources/static/js/Main.js
@@ -0,0 +1,7 @@
+function cambiar(){
+    document.getElementById("cohete").src = "Img/fuegos-artificiales.png"
+}
+
+function volver(){
+    document.getElementById("cohete").src= "Img/petardo.png"
+}
